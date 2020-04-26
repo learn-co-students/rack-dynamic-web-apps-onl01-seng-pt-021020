2ff70f2977c54d6633d3a4899c07603c9efae462
pbedrosian <peter_bedrosian@Yahoo.com>
2020-04-26 14:05:38 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,53 @@
+# class Application
+ 
+#   def call(env)
+#     resp = Rack::Response.new
+ 
+#     num_1 = Kernel.rand(1..20)
+#     num_2 = Kernel.rand(1..20)
+#     num_3 = Kernel.rand(1..20)
+ 
+#     resp.write "#{num_1}\n"
+#     resp.write "#{num_2}\n"
+#     resp.write "#{num_3}\n"
+ 
+#     if num_1==num_2 && num_2==num_3
+#       resp.write "You Win"
+#     else
+#       resp.write "You Lose"
+#     end
+ 
+#     resp.finish
+#   end
+ 
+# end
+
 class Application
+ 
+  @@items = ["Apples","Carrots","Pears"]
  
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
  
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
+      search_term = req.params["q"]
  
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
+ 
     else
-      resp.write "You Lose"
+      resp.write "Path Not Found"
     end
  
     resp.finish
   end
- 
 end
